eb3b9d43dab1e613f45d27196b25c7119cf12557
AtefBenH <ben_hasnaoui_atef@yahoo.fr>
2023-03-26 17:38:06 +0100

    Main

--- Python/fundamentals/oop/user.py
+++ Python/fundamentals/oop/user.py
@@ -26,18 +26,7 @@ class User :
         return self
     
 #Main
-user1 = User ("Walter White", "walter_white@gmail.com")
-user2 = User ("Keyser Soze", "keyser_soze@yahoo.fr")
-user3 = User ("Monkey-D Luffy", "luffy@aol.com")
-
-user1.make_deposit(1500).make_deposit(250).make_deposit(300).make_withdrawal(375).display_user_balance()
-
-user2.make_deposit(250).make_deposit(575).make_withdrawal(100).make_withdrawal(350).display_user_balance()
-
-user3.make_deposit(700).make_withdrawal(250).make_withdrawal(300).make_withdrawal(150).display_user_balance()
-
-user1.transfer_money(user3, 500).display_user_balance()
-user3.display_user_balance()
+#! Go to main.py to test
 
 
 
