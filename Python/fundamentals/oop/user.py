cc57e03143ec9787e861f6f2c98f1a9dea455d44
AtefBenH <ben_hasnaoui_atef@yahoo.fr>
2023-03-26 15:09:21 +0100

    Chaining Methods

--- Python/fundamentals/oop/user.py
+++ Python/fundamentals/oop/user.py
@@ -7,9 +7,11 @@ class User :
 
     def make_deposit (self, amount):
         self.account_balance+= amount
+        return self
 
     def make_withdrawal(self, amount):
         self.account_balance-= amount
+        return self
     
     def display_user_balance(self):
         print (f"User : {self.name}, Balance : {self.account_balance}")
@@ -17,32 +19,23 @@ class User :
     def transfer_money (self, user, amount):
         self.account_balance-= amount
         user.account_balance+= amount
+        print("--------------------------------------------------------------------")
+        print(f"{self.name} just sent {amount}$ to {user.name}")
+        print("--------------------------------------------------------------------")
+        return self
     
 #Main
 user1 = User ("Walter White", "walter_white@gmail.com")
 user2 = User ("Keyser Soze", "keyser_soze@yahoo.fr")
 user3 = User ("Monkey-D Luffy", "luffy@aol.com")
 
-user1.make_deposit(200)
-user1.make_deposit(150)
-user1.make_deposit(300)
-user1.make_withdrawal(175)
-user1.display_user_balance()
+user1.make_deposit(1500).make_deposit(250).make_deposit(300).make_withdrawal(375).display_user_balance()
 
-user2.make_deposit(250)
-user2.make_deposit(175)
-user2.make_withdrawal(100)
-user2.make_withdrawal(50)
-user2.display_user_balance()
+user2.make_deposit(250).make_deposit(575).make_withdrawal(100).make_withdrawal(350).display_user_balance()
 
-user3.make_deposit(300)
-user3.make_withdrawal(150)
-user3.make_withdrawal(200)
-user3.make_withdrawal(50)
-user3.display_user_balance()
+user3.make_deposit(700).make_withdrawal(250).make_withdrawal(300).make_withdrawal(150).display_user_balance()
 
-user1.transfer_money(user3, 200)
-user1.display_user_balance()
+user1.transfer_money(user3, 500).display_user_balance()
 user3.display_user_balance()
 
 
