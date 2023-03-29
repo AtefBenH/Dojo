e603550040e5c263372efe3711ac0bf4ff389645
AtefBenH <ben_hasnaoui_atef@yahoo.fr>
2023-03-29 17:17:33 +0100

    DLL : add_to_back

---
+++ Python/fundamentals/extras/Doubly Linked Lists (DLL)/classes/dllist.py
@@ -0,0 +1,35 @@
+from classes.dlnode import DLNode
+
+class DLList :
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.len = 0
+
+    def add_to_back(self, value) :
+        #If the list is empty
+        if self.head == None :
+            new_node = DLNode(value)
+            self.len+=1
+            self.head = new_node
+            self.tail = new_node
+            return self
+        #List not empty
+        else :
+            new_node = DLNode(value)
+            self.len+=1
+            pointer = self.tail
+            pointer.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+            return self
+    
+    def print_values (self) :
+        pointer = self.head
+        if pointer != None :
+            while (pointer != None) :
+                print(pointer.value)
+                pointer = pointer.next
+        else :
+            print ("Empty List")
+        return self
---
+++ Python/fundamentals/extras/Doubly Linked Lists (DLL)/main.py
@@ -0,0 +1,5 @@
+from classes.dllist import DLList
+
+dllist = DLList()
+dllist.add_to_back(1).add_to_back(7).add_to_back(3).print_values()
+# print(dllist.len)
