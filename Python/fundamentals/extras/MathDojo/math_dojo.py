5aeeb44903c7cdc1ce144c44803d59f7b667d9b6
AtefBenH <ben_hasnaoui_atef@yahoo.fr>
2023-04-02 17:32:30 +0100

    TDD+MathDojo

--- Python/fundamentals/extras/MathDojo/math_dojo.py
+++ Python/fundamentals/extras/MathDojo/math_dojo.py
@@ -1,3 +1,4 @@
+import unittest
 class MathDojo:
     def __init__(self):
         self.result = 0
@@ -13,16 +14,30 @@ class MathDojo:
             self.result-= num
             return self
     
-# create instances :
-md = MathDojo()
-md1 = MathDojo()
-md2 = MathDojo()
+class AddTests (unittest.TestCase) :
+    def testOne(self) :
+        self.assertEqual(self.setUp().add(2).add(2,5,1).subtract(3,2).result, 5)
+    def testTwo(self) :
+        self.assertEqual(self.setUp().add(4,3,10).add(3,5,7,4).add(4).result, 40)
+    def testT(self) :
+        self.assertEqual(self.setUp().subtract(5,5).subtract(3,1).subtract(2,4).result, -20)
+    def setUp(self):
+        dojo = MathDojo()
+        return dojo
 
-# to test:
-x = md.add(2).add(2,5,1).subtract(3,2).result
-print(x)	# should print 5
-# run each of the methods a few more times and check the result!
-x= md1.add(4,3,10).add(3,5,7,4).add(4).result
-print(x)    # should print 40
-x= md2.subtract(5,5).subtract(3,1).subtract(2,4).result
-print(x)    # should print -20
+if __name__ == "__main__" :
+    unittest.main()
+
+# # create instances :
+# md = MathDojo()
+# md1 = MathDojo()
+# md2 = MathDojo()
+
+# # to test:
+# x = md.add(2).add(2,5,1).subtract(3,2).result
+# print(x)	# should print 5
+# # run each of the methods a few more times and check the result!
+# x= md1.add(4,3,10).add(3,5,7,4).add(4).result
+# print(x)    # should print 40
+# x= md2.subtract(5,5).subtract(3,1).subtract(2,4).result
+# print(x)    # should print -20
