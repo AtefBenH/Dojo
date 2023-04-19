5b01bf5ad161c5eec8dd6240e3c7cc20e9e7a2b8
AtefBenH <ben_hasnaoui_atef@yahoo.fr>
2023-04-19 08:21:38 +0100

    Flask assignmetns: Hello_world+routing

---
+++ Python/flask/fundamentals/understanding_routing/server.py
@@ -0,0 +1,36 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/dojo')
+def dojo():
+    return 'Dojo'
+
+@app.route('/say/<string:name>')
+def say(name):
+    #! ---------- 1st Method ---------
+    if any(char.isdigit() for char in name) :
+        return "You Should Give a Valid Name"
+    return f'Hi {name}'
+    #! ---------- 2nd Method ---------
+    # for char in name :
+    #     if char.isdigit() :
+    #         return "You Should Give a Valid Name"
+    # return f'Hi {name}'
+
+@app.route('/repeat/<int:times>/<name>')
+def repeat(times, name):
+    return f'Hi {name} <br>' *times
+        
+
+#Catches any route other than the ones specified
+@app.errorhandler(404) 
+def page_not_found(e):
+    return "Sorry! No response. Try again."
+
+if __name__=="__main__":
+    app.run(debug=True)
+
