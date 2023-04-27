3bc7943bf9b26c24f095ad4b22f45d255aecc5b2
AtefBenH <ben_hasnaoui_atef@yahoo.fr>
2023-04-27 01:46:45 +0100

    hobbies

--- Python/flask/fundamentals/dojo_survey/server.py
+++ Python/flask/fundamentals/dojo_survey/server.py
@@ -10,40 +10,33 @@ def index():
 
 @app.route('/process', methods=['POST'])
 def process():
-    #Debug
-    print(request.form)
+    
     hobbies = []
     session['name'] = request.form['name']
     session['gender'] = request.form['gender']
     session['location'] = request.form['location']
     session['language'] = request.form['language']
     session['comment'] = request.form['comment']
-
-    if 'hobby1' in request.form :
-        hobbies.append(request.form['hobby1'])
-    if 'hobby2' in request.form :
-        hobbies.append(request.form['hobby2'])
-    if 'hobby3' in request.form :
-        hobbies.append(request.form['hobby3'])
-    if 'hobby4' in request.form :
-        hobbies.append(request.form['hobby4'])
-    if 'hobby5' in request.form :
-        hobbies.append(request.form['hobby5'])
-    if 'hobby6' in request.form :
-        hobbies.append(request.form['hobby6'])
+    for i in range (1, 7):
+        if ('hobby'+str(i)) in request.form :
+            hobbies.append(request.form['hobby'+str(i)])
+        # if 'hobby2' in request.form :
+        #     hobbies.append(request.form['hobby2'])
+        # if 'hobby3' in request.form :
+        #     hobbies.append(request.form['hobby3'])
+        # if 'hobby4' in request.form :
+        #     hobbies.append(request.form['hobby4'])
+        # if 'hobby5' in request.form :
+        #     hobbies.append(request.form['hobby5'])
+        # if 'hobby6' in request.form :
+        #     hobbies.append(request.form['hobby6'])
 
     session['hobbies'] = hobbies
-
-    print(session['hobbies'], "*"*100)
-    print(hobbies)
     return redirect('/display')
 
 @app.route('/display')
 def display():
     return render_template('display.html')
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
