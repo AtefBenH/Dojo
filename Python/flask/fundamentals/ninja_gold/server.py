a4720e0ab8a24f42e3e427312be1cbaf19df240c
AtefBenH <ben_hasnaoui_atef@yahoo.fr>
2023-04-28 09:15:24 +0100

    dispatching-handler

--- Python/flask/fundamentals/ninja_gold/server.py
+++ Python/flask/fundamentals/ninja_gold/server.py
@@ -5,6 +5,40 @@ import datetime
 app = Flask(__name__)
 app.secret_key = 'NoSecrectOnGithub'
 
+def process_farm():
+    session['amount'] = random.randint(10, 20)
+    session['gold']+= session['amount']
+    session['activities'].append("<div class='text-success'>Earned " + str(session['amount']) + " from the Farm! (" + str(session['datetime']) + ")</div>")
+    return redirect('/')
+
+def process_cave():
+    session['amount'] = random.randint(5, 10)
+    session['gold']+= session['amount']
+    session['activities'].append("<div class='text-success'>Earned " + str(session['amount']) + " from the Cave! (" + str(session['datetime']) + ")</div>")
+    return redirect('/')
+
+def process_house():
+    session['amount'] = random.randint(2, 5)
+    session['gold']+= session['amount']
+    session['activities'].append("<div class='text-success'>Earned " + str(session['amount']) + " from the House! (" + str(session['datetime']) + ")</div>")
+    return redirect('/')
+
+def process_casino():
+    session['amount'] = random.randint(-50, 50)
+    session['gold']+= session['amount']
+    if session['amount']>= 0 :
+        session['activities'].append("<div class='text-success'>Entered a Casino and won " + str(session['amount']) + " Golds!!!! Yaaaah.. (" + str(session['datetime']) + ")</div>")
+    else:
+        session['activities'].append("<div class='text-danger'>Entered a Casino and lost " + str(session['amount']) + " Golds.... Outch.. (" + str(session['datetime']) + ")</div>")
+    return redirect('/')
+
+form_handlers = {
+    'farm': process_farm,
+    'cave': process_cave,
+    'house': process_house,
+    'casino': process_casino,
+}
+
 @app.route('/')
 def index():
     if 'gold' not in session:
@@ -13,6 +47,10 @@ def index():
         session['activities'] = []
     if 'moves' not in session:
         session['moves'] = 0
+    if 'datetime' not in session:
+        session['datetime']=''
+    if 'amount' not in session:
+        session['amount'] = 0
     return render_template('index.html')
 
 @app.route('/', methods=['POST'])
@@ -25,29 +63,15 @@ def process_money():
     if session['moves']<15 and session['gold']<500:
         session['moves']+=1
         session['datetime'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        if request.form['building'] == 'farm':
-            session['amount'] = random.randint(10, 20)
-            session['gold']+= session['amount']
-            session['activities'].append("<div class='text-success'>Earned " + str(session['amount']) + " from the Farm! (" + str(session['datetime']) + ")</div>")
-        if request.form['building'] == 'cave':
-            session['amount'] = random.randint(5, 10)
-            session['gold']+= session['amount']
-            session['activities'].append("<div class='text-success'>Earned " + str(session['amount']) + " from the Cave! (" + str(session['datetime']) + ")</div>")
-        if request.form['building'] == 'house':
-            session['amount'] = random.randint(2, 5)
-            session['gold']+= session['amount'] 
-            session['activities'].append("<div class='text-success'>Earned " + str(session['amount']) + " from the House! (" + str(session['datetime']) + ")</div>")
-        if request.form['building'] == 'casino':
-            session['amount'] = random.randint(-50, 50)
-
-            session['gold']+= session['amount']
-            if session['amount']>= 0 :
-                session['activities'].append("<div class='text-success'>Entered a Casino and won " + str(session['amount']) + " Golds!!!! Yaaaah.. (" + str(session['datetime']) + ")</div>")
-            else:
-                session['activities'].append("<div class='text-danger'>Entered a Casino and lost " + str(session['amount']) + " Golds.... Outch.. (" + str(session['datetime']) + ")</div>")
-
+        form_name = request.form.get('building')
+        handler = form_handlers.get(form_name)
+        if handler:
+            return handler()
+        else:
+            return ('Unknown Form')
+        
     return redirect('/')
-
+    
 
 
 
