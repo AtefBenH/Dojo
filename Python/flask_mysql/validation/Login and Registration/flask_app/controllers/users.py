78157ec3118cc581e4fa56215d1ff85d837c71e5
AtefBenH <ben_hasnaoui_atef@yahoo.fr>
2023-05-06 01:02:12 +0100

    Login Registration Assignment

--- Python/flask_mysql/validation/Login and Registration/flask_app/controllers/users.py
+++ Python/flask_mysql/validation/Login and Registration/flask_app/controllers/users.py
@@ -1,7 +1,43 @@
 from flask_app import app
 from flask import render_template, redirect, request, session, flash
 from flask_app.models.user import User
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/users/create', methods = ['POST'])
+def create():
+    if User.validate(request.form):
+        hashed_password = bcrypt.generate_password_hash(request.form['password'])
+        data = {
+            **request.form, 'password':hashed_password
+        }
+        session['user_id'] = User.add(data)
+        return redirect('/dashboard')
+    return redirect('/')
+
+@app.route('/login', methods = ['POST'])
+def login():
+    user_from_db = User.get_by_email({'email' : request.form['email']})
+    if user_from_db :
+        if not bcrypt.check_password_hash(user_from_db.password, request.form['password']):
+            flash("Invalid Email/Password","Login")
+            return redirect('/')
+        else :
+            session['user_id'] = user_from_db.id
+            return redirect ('/dashboard')
+    flash("Invalid Email/Password","Login")
+    return redirect('/')
+
+@app.route('/dashboard')
+def show_user():
+    return render_template('dashboard.html', user = User.get_by_id({'id' : session['user_id']}))
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+    
--- Python/flask_mysql/validation/Login and Registration/flask_app/models/user.py
+++ Python/flask_mysql/validation/Login and Registration/flask_app/models/user.py
@@ -11,47 +11,72 @@ class User:
         self.last_name = data['last_name']
         self.email = data['email']
         self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @staticmethod
+    def verify_password(str):
+        upper_in = False
+        number_in = False
+        
+        for char in str:
+            if char.isupper():
+                upper_in = True
+            if char.isdigit():
+                number_in = True
+        
+        if upper_in and number_in:
+            return True
+        else:
+            return False
+
 
     @staticmethod
     def validate(user):
         is_valid = True
         #First Name Validation
         if len(user['first_name']) <2:
-            flash("First Name Must Be At Least 2 Characters.")
+            flash("First Name Must Be At Least 2 Characters.", "Register")
             is_valid = False
         elif not user['first_name'].isalpha():
-            flash("First Name Must Be Only Letters.")
+            flash("First Name Must Be Only Letters.", "Register")
             is_valid = False
         #Last Name Validation
         if len(user['last_name']) <2:
-            flash("Last Name Must Be At Least 2 Characters.")
+            flash("Last Name Must Be At Least 2 Characters.", "Register")
             is_valid = False
         elif not user['last_name'].isalpha():
-            flash("Last Name Must Be Only Letters.")
+            flash("Last Name Must Be Only Letters.", "Register")
+            is_valid = False
         #Email Validation
         if len(user['email']) <1:
             flash("Empty Email Address.")
             is_valid = False
         elif not EMAIL_REGEX.match(user['email']): 
-            flash("Invalid Email Address!")
+            flash("Invalid Email Address!", "Register")
             is_valid = False
         elif User.get_by_email({'email' : user['email']}):
-            flash("Email Already Exists!")
+            flash("Email Already Exists!", "Register")
             is_valid = False
         #Password Validation
         if len(user['password']) <8:
             is_valid = False
-            flash("Password Must Have More Than 8 Characters")
+            flash("Password Must Have More Than 8 Characters", "Register")
+        elif not User.verify_password(user['password']) :
+            is_valid = False
+            flash("Password Must Contain At Least A Number And An Uppercase Character", "Register")
         elif user['password']!= user['confirm_password']:
             is_valid = False
-            flash("Password and Confirmation Doesn't Match")
+            flash("Password and Confirmation Doesn't Match", "Register")
         return is_valid
 
 
     @classmethod
     def add(cls, data):
-        query = """INSERT INTO users (first_name, last_name, email, password) Values 
-        (%(first_name)s), (%(last_name)s), (%(email)s), (%(password)s);"""
+        query = """
+            INSERT INTO users (first_name, last_name, email, password) 
+            VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);
+        """
         return connectToMySQL(DATABASE).query_db(query, data)
     
     @classmethod
@@ -62,6 +87,14 @@ class User:
     @classmethod
     def get_by_email(cls, data):
         query = "SELECT * FROM users WHERE email = (%(email)s);"
+        result = connectToMySQL(DATABASE).query_db(query, data)
+        if len(result) < 1:
+            return False
+        return cls(result[0])
+    
+    @classmethod
+    def get_by_id(cls, data):
+        query = "SELECT * FROM users WHERE id = (%(id)s);"
         result = connectToMySQL(DATABASE).query_db(query, data)
         if len(result) < 1:
             return False
